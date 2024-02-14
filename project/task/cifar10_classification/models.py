96a4f3e116bdcf9675c4c440102ff358e8f4ff4d
Grant <grant.wilkins371@gmail.com>
2024-02-14 15:28:14 +0000

    Loud errors

--- project/task/cifar10_classification/models.py
+++ project/task/cifar10_classification/models.py
@@ -1,6 +1,7 @@
 """Define our models, and training and eval functions."""
 
-from torch import nn
+from torch import nn, Tensor
+
 
 from project.types.common import IsolatedRNG
 from torchvision.models import resnet18
@@ -9,11 +10,26 @@ from torchvision.models import resnet18
 class Net(nn.Module):
     """A PyTorch model."""
 
-    def __init__(self, pretrained: bool = False, **kwargs):
-        super(Net, self).__init__()
-        self.model = resnet18(pretrained=pretrained, **kwargs)
+    def __init__(self, pretrained: bool = False) -> None:
+        """Initialize the model.
 
-    def forward(self, x):
+        Args:
+        pretrained: Whether to use a pretrained model.
+        """
+        super().__init__()
+        self.model = resnet18(pretrained=pretrained)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Forward pass of the model.
+
+        Args:
+        x: The input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            The output tensor.
+        """
         return self.model(x)
 
 
