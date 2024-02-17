c433f9018ce0f65c00f6f6e054990abbaecddf59
Pranav Talluri <32508525+satnavpt@users.noreply.github.com>
2024-02-17 14:28:08 +0000

    remove unnecessary redefinition

--- project/task/cifar10_classification/models.py
+++ project/task/cifar10_classification/models.py
@@ -1,37 +1,9 @@
 """Define our models, and training and eval functions."""
 
-import torch
-from torch import nn
-
 from torchvision.models import resnet18
 from project.utils.utils import lazy_config_wrapper
 from project.types.common import NetGen
 
-
-class Net(nn.Module):
-    """A PyTorch model."""
-
-    def __init__(self, num_classes: int = 10) -> None:
-        """Initialize the model.
-
-        Args:
-        num_classes: How many classes in the subsequent model.
-        """
-        super().__init__()
-        self.model = resnet18(num_classes=num_classes)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the model.
-
-        Args:
-        x: The input tensor.
-
-        Returns
-        -------
-        torch.Tensor
-            The output tensor.
-        """
-        return self.model(x)
-
+Net = resnet18
 
 get_net: NetGen = lazy_config_wrapper(Net)
