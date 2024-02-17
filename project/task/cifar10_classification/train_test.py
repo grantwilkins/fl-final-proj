f75bb1a0d1d45d3946d5971ec4642909907a6650
Pranav Talluri <32508525+satnavpt@users.noreply.github.com>
2024-02-17 14:34:19 +0000

    test train code for gna + resnet batchnorm patch for torch func

--- project/task/cifar10_classification/train_test.py
+++ project/task/cifar10_classification/train_test.py
@@ -1,26 +1,26 @@
-"""Default training and testing functions, local and federated."""
+"""CIFAR10 training and testing functions, local and federated."""
 
 from collections.abc import Sized
 from pathlib import Path
 from typing import cast
 
 import torch
-from flwr.common import NDArrays
 from pydantic import BaseModel
 from torch import nn
 from torch.utils.data import DataLoader
 
-from project.client.client import ClientConfig
-from project.fed.utils.utils import generic_set_parameters
-from project.types.common import (
-    FedDataloaderGen,
-    FedEvalFN,
-    IsolatedRNG,
-    NetGen,
-    OnFitConfigFN,
-    TestFunc,
+from project.task.default.train_test import get_fed_eval_fn as get_default_fed_eval_fn
+from project.task.default.train_test import (
+    get_on_evaluate_config_fn as get_default_on_evaluate_config_fn,
 )
-from project.utils.utils import obtain_device
+from project.task.default.train_test import (
+    get_on_fit_config_fn as get_default_on_fit_config_fn,
+)
+from project.types.common import IsolatedRNG
+
+from gauss_newton import GNA
+from tqdm import tqdm
+from functorch.experimental import replace_all_batch_norm_modules_
 
 
 class TrainConfig(BaseModel):
@@ -31,8 +31,8 @@ class TrainConfig(BaseModel):
     """
 
     device: torch.device
-    # epochs: int
-    # learning_rate: float
+    epochs: int
+    learning_rate: float
 
     class Config:
         """Setting to allow any types, including library ones like torch.device."""
@@ -40,12 +40,12 @@ class TrainConfig(BaseModel):
         arbitrary_types_allowed = True
 
 
-def train(
+def train(  # pylint: disable=too-many-arguments
     net: nn.Module,
     trainloader: DataLoader,
     _config: dict,
     _working_dir: Path,
-    rng_tuple: IsolatedRNG,
+    _rng_tuple: IsolatedRNG,
 ) -> tuple[int, dict]:
     """Train the network on the training set.
 
@@ -80,10 +80,43 @@ def train(
     config: TrainConfig = TrainConfig(**_config)
     del _config
 
+    replace_all_batch_norm_modules_(net)
     net.to(config.device)
     net.train()
 
-    return len(cast(Sized, trainloader.dataset)), {}
+    criterion = nn.CrossEntropyLoss()
+    optimizer = GNA(net.parameters(), lr=config.learning_rate, model=net)
+    # optimizer = torch.optim.SGD(
+    #     net.parameters(),
+    #     lr=config.learning_rate,
+    #     weight_decay=0.001,
+    # )
+
+    final_epoch_per_sample_loss = 0.0
+    num_correct = 0
+    for _ in range(config.epochs):
+        final_epoch_per_sample_loss = 0.0
+        num_correct = 0
+        for data, target in tqdm(trainloader):
+            data, target = (
+                data.to(
+                    config.device,
+                ),
+                target.to(config.device),
+            )
+            optimizer.zero_grad()
+            output = net(data)
+            loss = criterion(output, target)
+            final_epoch_per_sample_loss += loss.item()
+            num_correct += (output.max(1)[1] == target).clone().detach().sum().item()
+            loss.backward()
+            optimizer.step(data)
+
+    return len(cast(Sized, trainloader.dataset)), {
+        "train_loss": final_epoch_per_sample_loss
+        / len(cast(Sized, trainloader.dataset)),
+        "train_accuracy": float(num_correct) / len(cast(Sized, trainloader.dataset)),
+    }
 
 
 class TestConfig(BaseModel):
@@ -106,7 +139,7 @@ def test(
     testloader: DataLoader,
     _config: dict,
     _working_dir: Path,
-    rng_tuple: IsolatedRNG,
+    _rng_tuple: IsolatedRNG,
 ) -> tuple[float, int, dict]:
     """Evaluate the network on the test set.
 
@@ -127,6 +160,7 @@ def test(
         The random number generator state for the training.
         Use if you need seeded random behavior
 
+
     Returns
     -------
     Tuple[float, int, float]
@@ -144,137 +178,36 @@ def test(
     net.to(config.device)
     net.eval()
 
+    criterion = nn.CrossEntropyLoss()
+    correct, per_sample_loss = 0, 0.0
+
+    with torch.no_grad():
+        for images, labels in testloader:
+            images, labels = (
+                images.to(
+                    config.device,
+                ),
+                labels.to(config.device),
+            )
+            outputs = net(images)
+            per_sample_loss += criterion(
+                outputs,
+                labels,
+            ).item()
+            _, predicted = torch.max(outputs.data, 1)
+            correct += (predicted == labels).sum().item()
+
     return (
-        0.0,
+        per_sample_loss / len(cast(Sized, testloader.dataset)),
         len(cast(Sized, testloader.dataset)),
-        {},
+        {
+            "test_accuracy": float(correct) / len(cast(Sized, testloader.dataset)),
+        },
     )
 
 
-def get_fed_eval_fn(
-    net_generator: NetGen,
-    fed_dataloader_generator: FedDataloaderGen,
-    test_func: TestFunc,
-    _config: dict,
-    working_dir: Path,
-    rng_tuple: IsolatedRNG,
-) -> FedEvalFN | None:
-    """Get the federated evaluation function.
-
-    Parameters
-    ----------
-    net_generator : NetGenerator
-        The function to generate the network.
-    fed_dataloader_generator : DataLoader
-        The DataLoader containing the data to test the network on.
-    test_func : TestFunc
-        The function to evaluate the network.
-    _config : Dict
-        The configuration for the testing.
-        Contains the device.
-        Static type checking is done by the TestConfig class.
-    working_dir : Path
-        The working directory for the training.
-    _rng_tuple : IsolatedRNGTuple
-        The random number generator state for the training.
-        Use if you need seeded random behavior
-
-    Returns
-    -------
-    Optional[FedEvalFN]
-        The evaluation function for the server
-        if the testloader is not empty, else None.
-    """
-    config: ClientConfig = ClientConfig(**_config)
-    del _config
-
-    testloader = fed_dataloader_generator(
-        True,
-        config.dataloader_config,
-        rng_tuple,
-    )
-
-    def fed_eval_fn(
-        _server_round: int,
-        parameters: NDArrays,
-        fake_config: dict,
-    ) -> tuple[float, dict] | None:
-        """Evaluate the model on the given data.
-
-        Parameters
-        ----------
-        server_round : int
-            The current server round.
-        parameters : NDArrays
-            The parameters of the model to evaluate.
-        _config : Dict
-            The configuration for the evaluation.
-
-        Returns
-        -------
-        Optional[Tuple[float, Dict]]
-            The loss and the accuracy of the input model on the given data.
-        """
-        net = net_generator(config.net_config, rng_tuple)
-        generic_set_parameters(net, parameters)
-        config.run_config["device"] = obtain_device()
-
-        if len(cast(Sized, testloader.dataset)) == 0:
-            return None
-
-        loss, _num_samples, metrics = test_func(
-            net,
-            testloader,
-            config.run_config,
-            working_dir,
-            rng_tuple,
-        )
-        return loss, metrics
-
-    return fed_eval_fn
-
-
-def get_on_fit_config_fn(fit_config: dict) -> OnFitConfigFN:
-    """Generate on_fit_config_fn based on a dict from the hydra config,.
-
-    Parameters
-    ----------
-    fit_config : Dict
-        The configuration for the fit function.
-        Loaded dynamically from the config file.
-    rng_tuple : IsolatedRNGTuple
-        The random number generator state for the training.
-        Use if you need seeded random behavior
-
-    Returns
-    -------
-    Optional[OnFitConfigFN]
-        The on_fit_config_fn for the server if the fit_config is not empty, else None.
-    """
-    # Fail early if the fit_config does not match expectations
-    ClientConfig(**fit_config)
-
-    def fit_config_fn(server_round: int) -> dict:
-        """CIFAR10 on_fit_config_fn.
-
-        Parameters
-        ----------
-        server_round : int
-            The current server round.
-            Passed to the client
-
-        Returns
-        -------
-        Dict
-            The configuration for the fit function.
-            Loaded dynamically from the config file.
-        """
-        fit_config["extra"]["server_round"] = server_round
-        return fit_config
-
-    return fit_config_fn
-
-
-# Differences between the two will come
-# from the config file
-get_on_evaluate_config_fn = get_on_fit_config_fn
+# Use defaults as they are completely determined
+# by the other functions defined in cifar10_classification
+get_fed_eval_fn = get_default_fed_eval_fn
+get_on_fit_config_fn = get_default_on_fit_config_fn
+get_on_evaluate_config_fn = get_default_on_evaluate_config_fn
