db8d284a7d30a98b4959f9bc80e4e59053ab1e95
Grant <grant.wilkins371@gmail.com>
2024-03-20 17:46:55 +0000

    fmnist.csv init

--- run_scripts/fmnist.sh
+++ run_scripts/fmnist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Add your code for the job manager
 
-# poetry run python -m project.main --config-name=fmnist-sgd
+poetry run python -m project.main --config-name=fmnist-sgd
 
 poetry run python -m project.main --config-name=fmnist-adam
 
