62f9df134ea0ef6c6f644c93849b7f4a54a92294
Grant <grant.wilkins371@gmail.com>
2024-03-20 14:11:43 +0000

    making some output public

--- run_scripts/fmnist.sh
+++ run_scripts/fmnist.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Add your code for the job manager
 
-poetry run python -m project.main --config-name=cifar10-sgd
+# poetry run python -m project.main --config-name=fmnist-sgd
 
-poetry run python -m project.main --config-name=cifar10-adam
+poetry run python -m project.main --config-name=fmnist-adam
 
-poetry run python -m project.main --config-name=cifar10-dgnmc
+poetry run python -m project.main --config-name=fmnist-dgnmc
 
-poetry run python -m project.main --config-name=cifar10-dgnexact
+poetry run python -m project.main --config-name=fmnist-dgnexact
 
-poetry run python -m project.main --config-name=cifar10-bdgnmc
+poetry run python -m project.main --config-name=fmnist-bdgnmc
 
-poetry run python -m project.main --config-name=cifar10-bdgnexact
+poetry run python -m project.main --config-name=fmnist-bdgnexact
 
-poetry run python -m project.main --config-name=cifar10-lbfgs
+poetry run python -m project.main --config-name=fmnist-lbfgs
